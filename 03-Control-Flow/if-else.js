9616d74bd87a4bdbd3df11ce3956b64d2a9d51ef
rajvircodes <rajvirsinhgohil004@gmail.com>
2025-09-16 13:53:57 +0530

    if else

---
+++ 03-Control-Flow/if-else.js
@@ -0,0 +1,12 @@
+let temprature = 25;
+// Example 1: using if-else statement
+if(temprature > 30){
+    console.log("It's a hot day");
+    
+}else if(temprature >= 20){
+    console.log("It's a pleasant day.");
+    
+}else {
+    console.log("It's a bit chilly.");
+    
+}
---
+++ 03-Control-Flow/switch.js
@@ -0,0 +1,14 @@
+// Ḥandle different cases 
+let day = "Monday";
+
+switch(day){
+    case "Monday":
+        console.log("Start of the work week.");
+        break;
+    case "Friday": 
+        console.log("Time to relax!");
+        break;
+    default :
+        console.log("Just a regular day");
+        
+}
